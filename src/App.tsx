ed33e3609aae88a5ea24196a85d043f64a92427e
unknown <minhaoguang@sutpc.com>
2021-09-28 18:28:50 +0800

    feat: menu缁勪欢step1鈥鈥

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0} onSelect={(index) => {alert(index)}}>
+          <MenuItem index={0}>a</MenuItem>
+          <MenuItem index={1} disabled>b</MenuItem>
+          <MenuItem index={2}>c</MenuItem>
+          <MenuItem index={3}>d</MenuItem>
+        </Menu>
+
+
         <Button onClick={(e) => {e.preventDefault();alert('fxpeter')}}>Hello</Button>
         <Button disabled>Disabled</Button>
 
