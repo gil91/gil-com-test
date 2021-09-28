fbb2bfd52d3bab742a440c03e96fca1a3b49694a
unknown <minhaoguang@sutpc.com>
2021-09-28 11:06:36 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>h1</h1>
-        <h2>h2</h2>
-        <code>console.log('fxpter')</code>
+        <Button onClick={(e) => {e.preventDefault();alert('fxpeter')}}>Hello</Button>
+        <Button disabled>Disabled</Button>
+
+        <Button
+          btnType={ButtonType.Primary}
+          size={ButtonSize.Large}
+        >
+          Primary Large
+        </Button>
+        <Button
+          btnType={ButtonType.Danger}
+          size={ButtonSize.Small}
+        >
+          Danger Small
+        </Button>
+        <Button 
+          btnType={ButtonType.Link} 
+          href="https://www.baidu.com"
+          target="_blank"
+        >
+          baidu
+        </Button>
+        <Button 
+          btnType={ButtonType.Link} 
+          href="https://www.baidu.com"
+          disabled
+        >
+          baidu
+        </Button>
       </header>
     </div>
   );
